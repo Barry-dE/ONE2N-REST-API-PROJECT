22f828f73e087635e17ef6c04780cf0a3d783c50
Barigbue <barigbuenbira@gmail.com>
2025-01-08 18:19:58 +0100

    Refactor: Load environment variables from .env file instead of hard-coding

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -3,13 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/Barry-dE/ONE2N-REST-API-PROJECT/internal/env"
 	"github.com/gin-gonic/gin"
 )
 
 func (app *application) healthCheckHandler(c *gin.Context) {
 	data := gin.H{
 		"status": "ok",
-		"env":    "development",
+		"env":    env.GetString("ENV", "Development"),
 	}
 
 	c.JSON(http.StatusOK, data)
