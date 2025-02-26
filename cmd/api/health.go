1afdef619d62c620cd38efc9a1b81d4b2dded4b0
Barigbue <barigbuenbira@gmail.com>
2025-02-26 21:50:54 +0100

    implement health check for API

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 
-	"github.com/Barry-dE/ONE2N-REST-API-PROJECT/internal/env"
 	"github.com/gin-gonic/gin"
 )
 
-func (app *application) healthCheckHandler(c *gin.Context) {
-	data := gin.H{
-		"status": "ok",
-		"env":    env.GetString("ENV", "Development"),
+const (
+	statusHealthy   = "healthy"
+	statusUnhealthy = "unhealthy"
+	statusUp        = "UP"
+	statusDown      = "DOWN"
+)
+
+type healthCheckResponse struct {
+	Status         string `json:"status"`
+	Env            string `json:"env"`
+	DatabaseStatus string `json:"database_status"`
+	TimeStamp      string `json:"timestamp"`
+	// TO-DO: Redis status
+}
+
+func checkDatabaseStatus(db *sql.DB) (string, error) {
+	if err := db.Ping(); err != nil {
+		return statusDown, err
 	}
 
-	c.JSON(http.StatusOK, data)
+	return statusUp, nil
+}
+
+func (app *application) healthCheckHandler(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		dbStatus, err := checkDatabaseStatus(db)
+		status := statusHealthy
+		if err != nil {
+			status = statusUnhealthy
+		}
+
+		healthData := healthCheckResponse{
+			Status:         status,
+			Env:            app.config.env,
+			DatabaseStatus: dbStatus,
+			TimeStamp:      time.Now().Format(time.RFC3339),
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": healthData,
+		})
+	}
 }
